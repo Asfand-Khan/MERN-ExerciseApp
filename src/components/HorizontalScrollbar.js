1fbd1f0d2f9891ec5a9fd5f3c50c3c6bb7fcb0d1
Asfand-Khan <khanasfand174@gmail.com>
2023-03-02 21:41:49 +0500

    complete search component and display respected results

---
+++ src/components/HorizontalScrollbar.js
@@ -0,0 +1,18 @@
+import { Box } from '@mui/material'
+import React from 'react'
+import BodyPart from './BodyPart';
+import { ScrollMenu,VisibilityContext } from 'react-horizontal-scrolling-menu';
+
+const HorizontalScrollbar = ({data,bodyPart,setBodyPart}) => {
+  return (
+    <ScrollMenu>
+        {data.map(item=>(
+            <Box key={item.id || item} itemID={item.id || item } title={item.id || item}>
+                <BodyPart item={item} bodyPart={bodyPart} setBodyPart={setBodyPart} />
+            </Box>
+        ))}
+    </ScrollMenu>
+  )
+}
+
+export default HorizontalScrollbar
---
+++ src/utils/fetchData.js
@@ -0,0 +1,14 @@
+export const exerciseOptions = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': 'd75e0c86a2mshb694b704ad56b2ap144bcbjsnbd38d216d5dc',
+      'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+    }
+  };
+
+export const fetchData = async(url,options)=>{
+    const response = await fetch(url,options);
+    const data = await response.json();
+
+    return data;
+}
